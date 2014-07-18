67ece6bf172814ca20eba9d32a550b1fbf76ee25
mauro.digiovanni <mauris86@gmail.com>
2014-07-18 17:53:01 +0200

    a

--- LoginApp-SpringMVC-Part1/src/com/pointerunits/web/LoginController.java
+++ LoginApp-SpringMVC-Part1/src/com/pointerunits/web/LoginController.java
@@ -5,7 +5,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
-
+/**
+ * 
+ * @author mauro.digiovanni
+ *
+ */
 @Controller
 @RequestMapping("/Login.spring")
 public class LoginController {
